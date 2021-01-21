1620e2d760e8ebab87114e8b7562c49cb58f1e69
Shriram Ravi <shriram@uable.com>
2021-01-21 13:55:01 +0530

    Updates

---
+++ precache-manifest.fd4e030376e2eed0582e105ff6350955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcde53cddfd01b320978518af33ec7f2",
+    "url": "/medium-blogs-react/index.html"
+  },
+  {
+    "revision": "2ca4276f65de0b61fb79",
+    "url": "/medium-blogs-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ab4af213ce22811f9563",
+    "url": "/medium-blogs-react/static/css/main.a0b31afb.chunk.css"
+  },
+  {
+    "revision": "2ca4276f65de0b61fb79",
+    "url": "/medium-blogs-react/static/js/2.f28e266f.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/medium-blogs-react/static/js/2.f28e266f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4af213ce22811f9563",
+    "url": "/medium-blogs-react/static/js/main.c8c1a396.chunk.js"
+  },
+  {
+    "revision": "789c4b97c3ab09b81565",
+    "url": "/medium-blogs-react/static/js/runtime-main.8b71c057.js"
+  }
+]);
