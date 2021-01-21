25317632de8ea5664e372fc4a67d24c83f27e404
Shriram Ravi <shriram@uable.com>
2021-01-21 13:39:50 +0530

    first commit

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar(props) {
+  return (
+    <>
+      <ul className="ullist">
+        
+      </ul>
+    </>
+  );
+}
+
+export default Navbar;
